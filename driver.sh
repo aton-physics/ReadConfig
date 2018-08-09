c7b2c9da6530afbd243d74c60f59c6468fb98161
Andrew Ton <aton@ted.hetchem.brown.edu>
2018-08-09 17:47:58 -0400

    Driver submits a job, pipes the trajectory on stdout to xz, and prints a trajectory labeled by grabbing the environment variable associated w the grid engine task manager

--- driver.sh
+++ driver.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-#unzip, send to ReadConfig, write to fstreams
-./executable -s | xz > somefile.data.xz
+#$ -S /bin/bash
+#$ -o /home/aton/ReadConfig/output
+#$ -wd /home/aton/ReadConfig
+#$ -b y
+#$ -j y
+#$ -N ZipGenCfg
+#$ -m ae
+#$ -M atatat123@gmail.com
+#$ -cwd
+#$ -v LD_LIBRARY_PATH="/stratt/vale/toor/lib:/stratt/vale/toor/gcc62/lib64"
+#
+
+/home/aton/ReadConfig/Trajectory.exe -s | xz > "/home/aton/ReadConfig/Trajectory/75/Trajectory$SGE_TASK_ID.xz"
